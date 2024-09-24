55d6d5d52ee75ccc41fb1291477167509c166c93
Yash Vachhani <yashvachhani1062@gmail.com>
2024-09-24 14:18:27 +0530

    bugs fixed

--- Assets/Scripts/GravityController.cs
+++ Assets/Scripts/GravityController.cs
@@ -11,6 +11,11 @@ public class GravityController : MonoBehaviour
     void Start()
     {
         skinnedMeshRenderers = GetComponentsInChildren<SkinnedMeshRenderer>();
+
+        foreach (SkinnedMeshRenderer renderer in skinnedMeshRenderers)
+        {
+            renderer.enabled = false;
+        }
     }
 
     // Update is called once per frame
@@ -21,6 +26,7 @@ public class GravityController : MonoBehaviour
         {
             shadowPosition();
             ToggleAllRenderers();
+            // Physics.gravity = new Vector3(0, -9.81f, 0);
         }
 
         if (Input.GetKey(KeyCode.UpArrow))
@@ -29,21 +35,20 @@ public class GravityController : MonoBehaviour
             shadow.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 180);
             shadow.transform.position += new Vector3(0,4,0);
             ToggleAllRenderers();
-            if(Input.GetKeyDown(KeyCode.Return)){
-                player.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 180);
-                // playerPosition();
-            }
+            // Physics.gravity = new Vector3(0, 9.81f, 0);
         }
+
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             shadowPosition();
             shadow.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z -90);
             shadow.transform.position += new Vector3(-3,2,0);
             ToggleAllRenderers();
-            if(Input.GetKeyDown(KeyCode.Return)){
-                player.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z-90);
-                // playerPosition();
-            }
+            // Physics.gravity = new Vector3(-9.81f, 0, 0);
+            // if(Input.GetKeyDown(KeyCode.Return)){
+            //     player.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z-90);
+            //     playerPosition();
+            // }
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
@@ -51,10 +56,14 @@ public class GravityController : MonoBehaviour
             shadow.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + 90);
             shadow.transform.position += new Vector3(3,2,0);
             ToggleAllRenderers();
-            if(Input.GetKeyDown(KeyCode.Return)){
-                player.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z+90);
-                // playerPosition();
-            }
+            // Physics.gravity = new Vector3(9.81f, 0, 0);
+            // if(Input.GetKeyDown(KeyCode.Return)){
+            //     player.transform.eulerAngles = new Vector3(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z+90);
+            // playerPosition();
+            // }
+        }
+        if(Input.GetKeyDown(KeyCode.Return)){
+                playerPosition();
         }
     }
 
@@ -63,6 +72,10 @@ public class GravityController : MonoBehaviour
         foreach (SkinnedMeshRenderer renderer in skinnedMeshRenderers)
         {
             renderer.enabled = !renderer.enabled;
+        }
+
+        if(Input.GetKeyDown(KeyCode.KeypadEnter)){
+                playerPosition();
         }
     }
 
